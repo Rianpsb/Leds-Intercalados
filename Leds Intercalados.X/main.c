28c677a737e119198632adf4c16615577ddf3d4b
19180091 <19180091@DSN1271137817.cfp127aluno.local>
2020-03-11 11:04:12 -0300

    Projeto finalizado (Podendo diminuir um pouco)

--- Leds Intercalados.X/main.c
+++ Leds Intercalados.X/main.c
@@ -4,11 +4,65 @@
  *
  * Created on 11 de Março de 2020, 08:51
  */
-
-
 #include <xc.h>
+#include "config.h" 
+#define LED1 PORTDbits.RD0
+#define DIR_LED1 TRISDbits.TRISD0
+#define LED2 PORTDbits.RD1
+#define DIR_LED2 TRISDbits.TRISD1
+#define S1 PORTDbits.RD3
+#define DIR_S1 TRISDbits.TRISD3
+#define S2 PORTDbits.RD4
+#define DIR_S2 TRISDbits.TRISD4
+#define ENTRADA 1
+#define SAIDA   0
 
-void main(void) {
+
+void main(void) 
+{
+    PORTD=0;
+    DIR_S1=ENTRADA;
+    DIR_S2=ENTRADA;
+    DIR_LED1=SAIDA;
+    DIR_LED2=SAIDA;
     
+    while(1)
+    {
+        if(S1==0 && S2==0)
+        {
+        LED1=1;
+        __delay_ms(500);
+        LED1=0;
+        __delay_ms(500);
+        LED2=1;
+        __delay_ms(500);
+        LED2=0;
+        __delay_ms(500);
+        }
+        
+        if(S1==1 && S2==1)
+        {
+        LED1=1;
+        __delay_ms(100);
+        LED1=0;
+        __delay_ms(100);
+        LED2=1;
+        __delay_ms(100);
+        LED2=0;
+        __delay_ms(100);
+        }
+     
+        if(S1==1 || S2==1)
+        {
+        LED1=1;
+        __delay_ms(250);
+        LED1=0;
+        __delay_ms(250);
+        LED2=1;
+        __delay_ms(250);
+        LED2=0;
+        __delay_ms(250);  
+        }    
+    }
     return;
 }
